da65cc8cdb7ec115aef61452de92471d7848cf7c
Benji Loya <benjiloya.apple@gmail.com>
2024-06-28 10:57:42 +0500

    Initial Commit

---
+++ SwiftTeam/SwiftTeamApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTeamApp.swift
+//  SwiftTeam
+//
+//  Created by Benji Loya on 28.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTeamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
